7d742cfec1260e449ee556ec483b8ed2e994e3fb
Jane Khomenko <31667552+JaneKKTTme@users.noreply.github.com>
2018-10-21 22:49:47 +0300

    The work is ready.

---
+++ NumbersApp/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace NumbersApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox4
+            // 
+            resources.ApplyResources(this.textBox4, "textBox4");
+            this.textBox4.Name = "textBox4";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2"),
+            resources.GetString("comboBox1.Items3"),
+            resources.GetString("comboBox1.Items4"),
+            resources.GetString("comboBox1.Items5"),
+            resources.GetString("comboBox1.Items6"),
+            resources.GetString("comboBox1.Items7"),
+            resources.GetString("comboBox1.Items8"),
+            resources.GetString("comboBox1.Items9"),
+            resources.GetString("comboBox1.Items10"),
+            resources.GetString("comboBox1.Items11"),
+            resources.GetString("comboBox1.Items12"),
+            resources.GetString("comboBox1.Items13"),
+            resources.GetString("comboBox1.Items14")});
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.Name = "comboBox1";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            resources.GetString("comboBox2.Items"),
+            resources.GetString("comboBox2.Items1"),
+            resources.GetString("comboBox2.Items2"),
+            resources.GetString("comboBox2.Items3"),
+            resources.GetString("comboBox2.Items4"),
+            resources.GetString("comboBox2.Items5"),
+            resources.GetString("comboBox2.Items6"),
+            resources.GetString("comboBox2.Items7"),
+            resources.GetString("comboBox2.Items8"),
+            resources.GetString("comboBox2.Items9"),
+            resources.GetString("comboBox2.Items10"),
+            resources.GetString("comboBox2.Items11"),
+            resources.GetString("comboBox2.Items12"),
+            resources.GetString("comboBox2.Items13"),
+            resources.GetString("comboBox2.Items14")});
+            resources.ApplyResources(this.comboBox2, "comboBox2");
+            this.comboBox2.Name = "comboBox2";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ NumbersApp/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.String;
+
+namespace NumbersApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }*/
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            if (IsNullOrWhiteSpace(textBox4.Text) || IsNullOrEmpty(comboBox1.Text) || IsNullOrEmpty(comboBox2.Text))
+                textBox1.Text = "Ошибка";
+            else
+            {
+                if (comboBox1.Text == comboBox2.Text)
+                    textBox1.Text = textBox4.Text;
+                /*else if (comboBox1.Text == "10" && Int32.Parse(comboBox2.Text) < 11) //10->2-9 system
+                {
+                    int number = Int32.Parse(textBox4.Text);
+                    int i = Int32.Parse(comboBox2.Text);
+                    int d = 1, result = 0;
+                    while (number > 0)
+                    {
+                        result += (number % i) * d;
+                        number = number / i;
+                        d = d * 10;
+                    }
+                    textBox1.Text = result.ToString();
+                }*/
+                else if (comboBox1.Text == "10") //10->another
+                {
+                    int number = Int32.Parse(textBox4.Text);
+                    int i = Int32.Parse(comboBox2.Text);
+                    int k = 0;
+                    while (number > 0)
+                    {
+                        number = number / i;
+                        k++;
+                    }
+                    char[] ch = new char[k];
+                    number = Int32.Parse(textBox4.Text);
+                    int result = 0;
+                    while (number > 0)
+                    {
+                        result = number % i;
+                        number = number / i;
+                        ch[k - 1] = IntToChar(result);
+                        k--;
+                    }
+                    textBox1.Text = new string(ch);
+                }
+                /*else if (comboBox2.Text == "10" && Int32.Parse(comboBox1.Text) < 11)
+                {
+                    int number = Int32.Parse(textBox4.Text);
+                    int i = Int32.Parse(comboBox1.Text);
+                    int result = anotherToTen(number, i);
+                    textBox1.Text = result.ToString();
+                }*/
+                else if (comboBox2.Text == "10")//another->10
+                {
+                    char[] ch = textBox4.Text.ToCharArray();
+                    int i = Int32.Parse(comboBox1.Text);
+                    int k = ch.Length, result = 0, d = 1;
+                    while (k != 0)
+                    {
+                        result += CharToInt(ch[k - 1]) * d;
+                        d = d * i;
+                        k--;
+                    }
+                    textBox1.Text = result.ToString();
+                }
+                else
+                {
+                    char[] ch = textBox4.Text.ToCharArray();
+                    int i = Int32.Parse(comboBox1.Text);
+                    int k = ch.Length, number = 0, d = 1;
+                    while (k != 0)
+                    {
+                        number += CharToInt(ch[k - 1]) * d;
+                        d = d * i;
+                        k--;
+                    }
+                    i = Int32.Parse(comboBox2.Text);
+                    k = 0;
+                    int number_ = number;
+                    while (number > 0)
+                    {
+                        number = number / i;
+                        k++;
+                    }
+                    char[] sh = new char[k];
+                    number = number_;
+                    int result = 0;
+                    while (number > 0)
+                    {
+                        result = number % i;
+                        number = number / i;
+                        sh[k - 1] = IntToChar(result);
+                        k--;
+                    }
+                    textBox1.Text = new string(sh);
+                }
+            }
+        }
+
+        private char IntToChar(int input)
+        {
+            switch(input)
+            {
+                case 1:
+                    return '1';
+                case 2:
+                    return '2';
+                case 3:
+                    return '3';
+                case 4:
+                    return '4';
+                case 5:
+                    return '5';
+                case 6:
+                    return '6';
+                case 7:
+                    return '7';
+                case 8:
+                    return '8';
+                case 9:
+                    return '9';
+                case 10:
+                    return 'A';
+                case 11:
+                    return 'B';
+                case 12:
+                    return 'C';
+                case 13:
+                    return 'D';
+                case 14:
+                    return 'E';
+                case 15:
+                    return 'F';
+                default:
+                    return '0';
+            }
+        }
+
+        private int CharToInt(char input)
+        {
+            switch (input)
+            {
+                case '1':
+                    return 1;
+                case '2':
+                    return 2;
+                case '3':
+                    return 3;
+                case '4':
+                    return 4;
+                case '5':
+                    return 5;
+                case '6':
+                    return 6;
+                case '7':
+                    return 7;
+                case '8':
+                    return 8;
+                case '9':
+                    return 9;
+                case 'A':
+                    return 10;
+                case 'B':
+                    return 11;
+                case 'C':
+                    return 12;
+                case 'D':
+                    return 13;
+                case 'E':
+                    return 14;
+                case 'F':
+                    return 15;
+                default:
+                    return 0;
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
