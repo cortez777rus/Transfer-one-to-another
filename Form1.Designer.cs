2f915f11282ae499b41dcd63c8f66647f1d6d802
Jane Khomenko <31667552+JaneKKTTme@users.noreply.github.com>
2018-11-03 01:24:38 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace NumbersApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // comboBox1
+            // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2"),
+            resources.GetString("comboBox1.Items3"),
+            resources.GetString("comboBox1.Items4"),
+            resources.GetString("comboBox1.Items5"),
+            resources.GetString("comboBox1.Items6"),
+            resources.GetString("comboBox1.Items7"),
+            resources.GetString("comboBox1.Items8"),
+            resources.GetString("comboBox1.Items9"),
+            resources.GetString("comboBox1.Items10"),
+            resources.GetString("comboBox1.Items11"),
+            resources.GetString("comboBox1.Items12"),
+            resources.GetString("comboBox1.Items13"),
+            resources.GetString("comboBox1.Items14")});
+            this.comboBox1.Name = "comboBox1";
+            // 
+            // comboBox2
+            // 
+            resources.ApplyResources(this.comboBox2, "comboBox2");
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            resources.GetString("comboBox2.Items"),
+            resources.GetString("comboBox2.Items1"),
+            resources.GetString("comboBox2.Items2"),
+            resources.GetString("comboBox2.Items3"),
+            resources.GetString("comboBox2.Items4"),
+            resources.GetString("comboBox2.Items5"),
+            resources.GetString("comboBox2.Items6"),
+            resources.GetString("comboBox2.Items7"),
+            resources.GetString("comboBox2.Items8"),
+            resources.GetString("comboBox2.Items9"),
+            resources.GetString("comboBox2.Items10"),
+            resources.GetString("comboBox2.Items11"),
+            resources.GetString("comboBox2.Items12"),
+            resources.GetString("comboBox2.Items13"),
+            resources.GetString("comboBox2.Items14")});
+            this.comboBox2.Name = "comboBox2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.String;
+
+namespace NumbersApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }*/
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private char IntToChar(int input)
+        {
+            switch (input)
+            {
+                case 0:
+                    return '0';
+                case 1:
+                    return '1';
+                case 2:
+                    return '2';
+                case 3:
+                    return '3';
+                case 4:
+                    return '4';
+                case 5:
+                    return '5';
+                case 6:
+                    return '6';
+                case 7:
+                    return '7';
+                case 8:
+                    return '8';
+                case 9:
+                    return '9';
+                case 10:
+                    return 'A';
+                case 11:
+                    return 'B';
+                case 12:
+                    return 'C';
+                case 13:
+                    return 'D';
+                case 14:
+                    return 'E';
+                case 15:
+                    return 'F';
+                default:
+                    return '0';
+            }
+        }
+
+        private int CharToInt(char input)
+        {
+            switch (input)
+            {
+                case '0':
+                    return 0;
+                case '1':
+                    return 1;
+                case '2':
+                    return 2;
+                case '3':
+                    return 3;
+                case '4':
+                    return 4;
+                case '5':
+                    return 5;
+                case '6':
+                    return 6;
+                case '7':
+                    return 7;
+                case '8':
+                    return 8;
+                case '9':
+                    return 9;
+                case 'A':
+                    return 10;
+                case 'B':
+                    return 11;
+                case 'C':
+                    return 12;
+                case 'D':
+                    return 13;
+                case 'E':
+                    return 14;
+                case 'F':
+                    return 15;
+                default:
+                    return 0;
+            }
+        }
+
+        private String tenToAnotherInt(int number, int s)
+        {
+            int number_ = number;
+            int k = 0;
+            while (number > 0)
+            {
+                number = number / s;
+                k++;
+            }
+            char[] ch = new char[k];
+            number = number_;
+            int result = 0;
+            while (number > 0)
+            {
+                result = number % s;
+                number = number / s;
+                ch[k - 1] = IntToChar(result);
+                k--;
+            }
+            String result_ = new string(ch);
+            return result_;
+        }
+
+        private String anotherToTenInt(char[] number, int s)
+        {
+            int k = number.Length, result = 0, d = 1;
+            while (k != 0)
+            {
+                result += CharToInt(number[k - 1]) * d;
+                d = d * s;
+                k--;
+            }
+            return result.ToString();
+        }
+
+        private String tenToAnotherDivisional(double number, int s)
+        {
+            double number_ = number;
+            int k = 0;
+            while ((number * s) % 1 != 0)
+            {
+                number = number * s - (number * s) / 1;
+                k++;
+            }
+            k++;
+            char[] ch = new char[k];
+            number = number_;
+            int result = 0;
+            int z = 0;
+            while ((number * s) % 1 != 0 && z < k)
+            {
+                result = (int)(number * s);
+                number = number * s - result;
+                ch[z] = IntToChar(result);
+                z++;
+            }
+            String result_ = new string(ch);
+            return result_;
+        }
+
+        private String anotherToTenDivisional(char[] number, int s)
+        {
+            int k = 0;
+            double result = 0, d = 1 % s;
+            while (k != number.Length)
+            {
+                result += CharToInt(number[k]) * d;
+                d = d / s;
+                k++;
+            }
+            return result.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = " ";
+            if (IsNullOrWhiteSpace(textBox1.Text) || IsNullOrEmpty(comboBox1.Text) || IsNullOrEmpty(comboBox2.Text))
+                textBox2.Text = "Ошибка";
+            else
+            {
+                if (comboBox1.Text == comboBox2.Text)
+                    textBox2.Text = textBox1.Text;
+                else if (comboBox1.Text == "10") //10->another
+                {
+                    int i = Int32.Parse(comboBox2.Text);
+                    int data_int = (int)Convert.ToDouble(textBox1.Text) / 1;
+                    if (Convert.ToDouble(textBox1.Text) - data_int > 0)
+                    {
+                        double data_divisional = Convert.ToDouble(textBox1.Text) - data_int;
+                        textBox2.Text = tenToAnotherInt(data_int, i) + ',' + tenToAnotherDivisional(data_divisional, i);//The result is an amount of integer part and divisional one
+                    }
+                    else if (Convert.ToDouble(textBox1.Text) - data_int == 0)
+                    {
+                        textBox2.Text = tenToAnotherInt(data_int, i);
+                    }
+                    else
+                        textBox2.Text = "Ошибка";
+                }
+                else if (comboBox2.Text == "10")//another->10
+                {
+                    char[] ch = textBox1.Text.ToCharArray();
+                    bool z = false;
+                    for (int m = 0; m <= ch.Length - 1; m++)
+                        if (ch[m] == ',')
+                            z = true;
+                    if (z == false)
+                    {
+                        char[] data_int = new char[ch.Length];
+                        data_int = ch;
+                        int i = Int32.Parse(comboBox1.Text);
+                        textBox2.Text = anotherToTenInt(data_int, i);
+                    }
+                    else if (z == true)
+                    {
+                        int n = 0;
+                        while (ch[n] != ',')
+                            n++;
+                        char[] data_int = new char[n];
+                        n = 0;
+                        while (ch[n] != ',')
+                        {
+                            data_int[n] = ch[n];
+                            n++;
+                        }
+                        n++;
+                        char[] data_divisional = new char[ch.Length - n];
+                        int k = 0;
+                        while (n != ch.Length)
+                        {
+                            data_divisional[k] = ch[n];
+                            n++;
+                            k++;
+                        }
+                        int i = Int32.Parse(comboBox1.Text);
+                        textBox2.Text = anotherToTenInt(data_int, i) + ',' + anotherToTenDivisional(data_divisional, i);
+                    }
+                }
+                else
+                {
+                    int i = Int32.Parse(comboBox1.Text);
+                    char[] ch = textBox1.Text.ToCharArray();
+                    bool z = false;
+                    for (int m = 0; m <= ch.Length - 1; m++)
+                        if (ch[m] == ',')
+                            z = true;
+                    if (z == false)
+                    {
+                        char[] data_int = new char[ch.Length];
+                        data_int = ch;
+                        int dat_int = Int32.Parse(anotherToTenInt(data_int, i));
+                        i = Int32.Parse(comboBox2.Text);
+                        textBox2.Text = tenToAnotherInt(dat_int, i);
+                    }
+                    else if (z == true)
+                    {
+                        int n = 0;
+                        while (ch[n] != ',')
+                            n++;
+                        char[] data_int = new char[n];
+                        n = 0;
+                        while (ch[n] != ',')
+                        {
+                            data_int[n] = ch[n];
+                            n++;
+                        }
+                        n++;
+                        char[] data_divisional = new char[ch.Length - n];
+                        int k = 0;
+                        while (n != ch.Length)
+                        {
+                            data_divisional[k] = ch[n];
+                            n++;
+                            k++;
+                        }
+                        String s = anotherToTenInt(data_int, i) + ',' + anotherToTenDivisional(data_divisional, i);
+                        int dat_int = (int)Convert.ToDouble(s) / 1;
+                        double dat_divisional = Convert.ToDouble(s) - dat_int;
+                        i = Int32.Parse(comboBox2.Text);
+                        textBox2.Text = tenToAnotherInt(dat_int, i) + ',' + tenToAnotherDivisional(dat_divisional, i);
+                    }
+                }
+            }
+        }
+    }
+}
